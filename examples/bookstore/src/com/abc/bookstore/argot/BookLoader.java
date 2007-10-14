847dab1065e5c6e98365112ef35c90fe7ad89b46
David Ryan <david@livemedia.com.au>
2007-10-14 10:37:50 +0000

    Remove un-needed imports

--- examples/bookstore/src/com/abc/bookstore/argot/BookLoader.java
+++ examples/bookstore/src/com/abc/bookstore/argot/BookLoader.java
@@ -1,18 +1,11 @@
 package com.abc.bookstore.argot;
 
 import com.abc.bookstore.Book;
-import com.abc.bookstore.BookNotFoundException;
-import com.abc.bookstore.Bookstore;
-import com.abc.bookstore.IBookstore;
-
 import com.argot.ResourceDictionaryLoader;
 import com.argot.TypeException;
 import com.argot.TypeLibrary;
-import com.argot.TypeReaderAuto;
 import com.argot.auto.TypeArrayMarshaller;
 import com.argot.auto.TypeBeanMarshaller;
-import com.argot.remote.MetaInterface;
-import com.argot.remote.MetaRemoteException;
 
 
 public class BookLoader 

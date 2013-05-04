1ad8eae60dbff9baae090c815328b42aa2dfc818
David Ryan <david@livemedia.com.au>
2013-05-04 12:59:45 +0000

    Add annotation for allowing automatic reading/writing of types.

---
+++ src/com/argot/auto/ArgotTag.java
@@ -0,0 +1,13 @@
+package com.argot.auto;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ArgotTag
+{
+	String value();
+}

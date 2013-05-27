2a1270bae24e48534a7e30ef6a40de3c53bd10a3
David Ryan <david@livemedia.com.au>
2013-05-27 12:21:16 +0000

    Update version

--- src/com/argot/TypeLibrary.java
+++ src/com/argot/TypeLibrary.java
@@ -118,7 +118,7 @@ public class TypeLibrary
 	
 	private void init()
 	{
-		System.out.println("\nArgot Version 1.3.b");
+		System.out.println("\nArgot Version 1.3.b2");
 		System.out.println("Copyright 2003-2013 (C) Live Media Pty Ltd.");
 		System.out.println("www.argot-sdk.org\n");
 		

d2920fa68ce972f5ffe75e2d4b0b2a1b46c70186
David Ryan <david@livemedia.com.au>
2013-05-30 10:54:16 +0000

    Removed bookstore example.  Not useful for Internet of Things.  Better examples coming Removed Remote, Peer, Message and Model code.  Some will go into extensions.

--- examples/bookstore/localBookstore.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-if exist "com\abc\bookstore\application\LocalBookstore.class" goto bookstoreBuilt
-@echo "Bookstore not built. Calling ANT"
-call ant
-:bookstoreBuilt
-
-java -cp "..\..\lib\argot-1.3.b.jar;." com.abc.bookstore.application.LocalBookstore

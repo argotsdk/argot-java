31fb237fc9605bb6e00e2587e60c7647db008ef9
David Ryan <david@livemedia.com.au>
2014-01-30 10:24:21 +0000

    Added additional error information.  Lots of formatting changes.

--- src/com/argot/meta/MetaAbstract.java
+++ src/com/argot/meta/MetaAbstract.java
@@ -60,19 +60,19 @@ implements MetaDefinition, TypeRelation
 
 	protected Map<Integer,MetaMap> _concreteToMap;
 
-	private Map<String,Integer> _relationMap;
-	
+	private final Map<String,Integer> _relationMap;
+
 	private MetaAbstractMap _defaultMappings[];
-	
+
 	// This keeps a list of any abstract types we've mapped.
-	private List<MetaAbstract> _abstractParentMap;
-	
+	private final List<MetaAbstract> _abstractParentMap;
+
 	protected class MetaMap
 	{
 		public boolean isHidden;
 		public int mapTypeId;
 	}
-	
+
 	public MetaAbstract()
 	{
 		_concreteToMap = new HashMap<Integer,MetaMap>();
@@ -81,39 +81,40 @@ implements MetaDefinition, TypeRelation
 		_abstractParentMap = new ArrayList<MetaAbstract>();
 		_defaultMappings = null;
 	}
-	
-	public MetaAbstract( Object mappings[] )
+
+	public MetaAbstract( final Object mappings[] )
 	{
 		this();
 		_defaultMappings = new MetaAbstractMap[mappings.length];
-		
+
 		for (int x=0; x<mappings.length;x++)
 		{
 			_defaultMappings[x] = (MetaAbstractMap) mappings[x];
 		}
 	}
-	
-	public MetaAbstract( MetaAbstractMap defaultMappings[] )
+
+	public MetaAbstract( final MetaAbstractMap defaultMappings[] )
 	{
 		this();
 		_defaultMappings = defaultMappings;
 	}
-	
-    public void bind(TypeLibrary library, int memberTypeId,	TypeLocation location, TypeElement definition) 
-    throws TypeException 
+
+    @Override
+    public void bind(final TypeLibrary library, final int memberTypeId,	final TypeLocation location, final TypeElement definition)
+    throws TypeException
     {
 		super.bind(library, memberTypeId, location, definition);
 		if (_defaultMappings != null)
 		{
 			for (int x=0; x<_defaultMappings.length;x++)
 			{
-				MetaAbstractMap mapping = _defaultMappings[x];
+				final MetaAbstractMap mapping = _defaultMappings[x];
 				if (mapping == null)
 				{
 					throw new TypeException("MetaAbstract: Default mapping " + x + " invalid");
 				}
-				
-		        TypeElement concreteElement = library.getStructure( mapping.getConcreteType() ); 
+
+		        final TypeElement concreteElement = library.getStructure( mapping.getConcreteType() );
 		        if (concreteElement instanceof MetaAbstract)
 		        {
 		        	((MetaAbstract)concreteElement).addAbstractMap(this);
@@ -127,59 +128,63 @@ implements MetaDefinition, TypeRelation
 		}
 	}
 
-	public String getTypeName()
+	@Override
+    public String getTypeName()
     {
         return TYPENAME;
     }
-    
-	
-	public int getConcreteId(int id)
+
+
+	public int getConcreteId(final int id)
 	{
-		MetaMap m = _concreteToMap.get(id);
-		if (m == null ) return -1;
+		final MetaMap m = _concreteToMap.get(id);
+		if (m == null ) {
+            return -1;
+        }
 		return m.mapTypeId;
 	}
-	
+
 	public Integer[] getConcreteIds()
 	{
 		return _concreteToMap.keySet().toArray(new Integer[_concreteToMap.keySet().size()]);
 	}
 
-    public void addAbstractMap( MetaAbstract parent )
+    public void addAbstractMap( final MetaAbstract parent )
     {
     	_abstractParentMap.add( parent );
-    	
-    	Iterator<Integer> i = _concreteToMap.keySet().iterator();
+
+    	final Iterator<Integer> i = _concreteToMap.keySet().iterator();
     	while( i.hasNext())
     	{
-    		Integer c = i.next();
-    		MetaMap m = (MetaMap) _concreteToMap.get(c);
-    		
+    		final Integer c = i.next();
+    		final MetaMap m = _concreteToMap.get(c);
+
     		parent.addMap( c.intValue(), m.mapTypeId, m.isHidden);
     	}
-    	
+
     }
-    
-	public void addMap( int concreteType, int mapType, boolean isHidden )
+
+	public void addMap( final int concreteType, final int mapType, final boolean isHidden )
 	{
-		MetaMap metaMap = new MetaMap();
+		final MetaMap metaMap = new MetaMap();
 		metaMap.isHidden = isHidden;
 		metaMap.mapTypeId = mapType;
-		
+
 		_concreteToMap.put( new Integer( concreteType ), metaMap);
-		
-    	Iterator<MetaAbstract> i = _abstractParentMap.iterator();
+
+    	final Iterator<MetaAbstract> i = _abstractParentMap.iterator();
     	while( i.hasNext())
     	{
-    		MetaAbstract p = i.next();
-    		
+    		final MetaAbstract p = i.next();
+
     		p.addMap( concreteType, mapType, isHidden);
-    	}		
+    	}
 	}
-	
-	public int getRelation(String tag) 
+
+	@Override
+    public int getRelation(final String tag)
 	{
-		Integer i = (Integer) _relationMap.get(tag);
+		final Integer i = _relationMap.get(tag);
 		if (i == null)
 		{
 			return TypeLibrary.NOTYPE;
@@ -187,35 +192,40 @@ implements MetaDefinition, TypeRelation
 		return i.intValue();
 	}
 
-	public void setRelation(String tag, int id) 
+	@Override
+    public void setRelation(final String tag, final int id)
 	{
 		_relationMap.put(tag, new Integer(id));
 	}
-	
-	
+
+
 	public static class MetaAbstractTypeReader
 	implements TypeReader, TypeBound, TypeLibraryReader, MetaExpressionReader
 	{
-		private MetaMarshaller _reader;
-		
+		private final MetaMarshaller _reader;
+
 		public MetaAbstractTypeReader()
 		{
 			_reader = new MetaMarshaller();
 		}
-		
-		public void bind(TypeLibrary library, int definitionId, TypeElement definition) 
-		throws TypeException 
+
+		@Override
+        public void bind(final TypeLibrary library, final int definitionId, final TypeElement definition)
+		throws TypeException
 		{
 			_reader.bind(library, definitionId, definition);
 		}
-		
-	    public Object read(TypeInputStream in) 
+
+	    @Override
+        public Object read(final TypeInputStream in)
 	    throws TypeException, IOException
 	    {
-			Object[] object = (Object[]) _reader.getReader(in.getTypeMap()).read(in);
-			if (object.length != 1 && !(object[0] instanceof Object[])) throw new TypeException("Unexpected data");
-			Object[] mapArray = (Object[]) object[0];
-			MetaAbstractMap[] defaultMaps = new MetaAbstractMap[mapArray.length];
+			final Object[] object = (Object[]) _reader.getReader(in.getTypeMap()).read(in);
+			if (object.length != 1 && !(object[0] instanceof Object[])) {
+                throw new TypeException("Unexpected data");
+            }
+			final Object[] mapArray = (Object[]) object[0];
+			final MetaAbstractMap[] defaultMaps = new MetaAbstractMap[mapArray.length];
 			for (int x=0;x<mapArray.length;x++)
 			{
 				defaultMaps[x] = (MetaAbstractMap) mapArray[x];
@@ -225,16 +235,18 @@ implements MetaDefinition, TypeRelation
 			return new MetaAbstract(defaultMaps);
 	    }
 
-		public TypeReader getReader(TypeMap map) 
-		throws TypeException 
+		@Override
+        public TypeReader getReader(final TypeMap map)
+		throws TypeException
 		{
 			return this;
 		}
 
-		public TypeReader getExpressionReader(TypeMap map, MetaExpressionResolver resolver, TypeElement element)
-		throws TypeException 
+		@Override
+        public TypeReader getExpressionReader(final TypeMap map, final MetaExpressionResolver resolver, final TypeElement element)
+		throws TypeException
 		{
-			MetaAbstract metaAbstract = (MetaAbstract) element;
+			final MetaAbstract metaAbstract = (MetaAbstract) element;
 			return new MetaAbstractReader(metaAbstract,map.getReader(map.getStreamId(UVInt28.TYPENAME)));
 		}
 	}
@@ -242,35 +254,38 @@ implements MetaDefinition, TypeRelation
 	public static class MetaAbstractTypeWriter
 	implements TypeWriter, TypeLibraryWriter, MetaExpressionWriter
 	{
-		
-	    public void write(TypeOutputStream out, Object o) 
+
+	    @Override
+        public void write(final TypeOutputStream out, final Object o)
 	    throws TypeException, IOException
 	    {
-	    	MetaAbstract ma = (MetaAbstract) o;
-	    	
+	    	final MetaAbstract ma = (MetaAbstract) o;
+
 	    	// MetaAbstract uses UVInt28 as part of reading a data type.  This will ensure the type is mapped.
-	    	TypeMap refMap = (TypeMap) out.getTypeMap().getReference(TypeMap.REFERENCE_MAP);
+	    	final TypeMap refMap = (TypeMap) out.getTypeMap().getReference(TypeMap.REFERENCE_MAP);
 	    	refMap.getStreamId(UVInt28.TYPENAME);
-	    	
+
 	    	out.writeObject(UInt8.TYPENAME, new Integer(ma._defaultMappings.length));
-	    	
-	    	MetaAbstractMap[] defaultMappings = ma._defaultMappings;
+
+	    	final MetaAbstractMap[] defaultMappings = ma._defaultMappings;
 	    	for (int x=0; x<defaultMappings.length;x++)
 	    	{
 	    		out.writeObject(MetaAbstractMap.TYPENAME, defaultMappings[x]);
 	    	}
 	    }
-	    
-		public TypeWriter getWriter(TypeMap map) 
-		throws TypeException 
+
+		@Override
+        public TypeWriter getWriter(final TypeMap map)
+		throws TypeException
 		{
 			return this;
 		}
 
-		public TypeWriter getExpressionWriter(TypeMap map, MetaExpressionResolver resolver, TypeElement element)
-		throws TypeException 
+		@Override
+        public TypeWriter getExpressionWriter(final TypeMap map, final MetaExpressionResolver resolver, final TypeElement element)
+		throws TypeException
 		{
-			MetaAbstract metaAbstract = (MetaAbstract) element;
+			final MetaAbstract metaAbstract = (MetaAbstract) element;
 			return new MetaAbstractWriter(metaAbstract,map.getWriter(map.getStreamId(UVInt28.TYPENAME)));
 		}
 	}
@@ -278,40 +293,41 @@ implements MetaDefinition, TypeRelation
 	public static class MetaAbstractReader
 	implements TypeReader
 	{
-		private MetaAbstract _metaAbstract;
-		private TypeReader _uvint28;
-		private Map<Integer,TypeReader> _mapCache;
-		
-		public MetaAbstractReader(MetaAbstract metaAbstract, TypeReader uvint28)
+		private final MetaAbstract _metaAbstract;
+		private final TypeReader _uvint28;
+		private final Map<Integer,TypeReader> _mapCache;
+
+		public MetaAbstractReader(final MetaAbstract metaAbstract, final TypeReader uvint28)
 		{
 			_metaAbstract = metaAbstract;
 			_uvint28 = uvint28;
 			_mapCache = new HashMap<Integer,TypeReader>();
 		}
-		
-		public Object read(TypeInputStream in)
-		throws TypeException, IOException 
+
+		@Override
+        public Object read(final TypeInputStream in)
+		throws TypeException, IOException
 		{
-	        Integer type = (Integer) _uvint28.read(in);
-	        TypeReader mappedReader = (TypeReader) _mapCache.get(type);
+	        final Integer type = (Integer) _uvint28.read(in);
+	        TypeReader mappedReader = _mapCache.get(type);
 	        if ( mappedReader == null )
 	        {
-	        	int id = mapType( in.getTypeMap(), type );
+	        	final int id = mapType( in.getTypeMap(), type );
 	        	mappedReader = in.getTypeMap().getReader(id);
 	        	_mapCache.put(type, mappedReader);
-	        }	        
+	        }
 			return mappedReader.read( in );
 		}
-		
-		private int mapType(TypeMap map, Integer type)
+
+		private int mapType(final TypeMap map, final Integer type)
 		throws TypeException
 		{
-	        int definitionId = map.getDefinitionId( type.intValue() );
-	        TypeLocation location = map.getLibrary().getLocation(definitionId);
+	        final int definitionId = map.getDefinitionId( type.intValue() );
+	        final TypeLocation location = map.getLibrary().getLocation(definitionId);
 	        int identityId = -1;
 	        if (location instanceof TypeLocationDefinition)
 	        {
-	        	TypeLocationDefinition definition = (TypeLocationDefinition) location;
+	        	final TypeLocationDefinition definition = (TypeLocationDefinition) location;
 	        	identityId = definition.getId();
 	        }
 	        else if (location instanceof TypeLocationName)
@@ -322,19 +338,19 @@ implements MetaDefinition, TypeRelation
 	        {
 	        	identityId = definitionId;
 	        }
-	        else 
+	        else
 	        {
 	        	throw new TypeException("Unknown location type");
 	        }
-	        
-			MetaMap concrete = (MetaMap) _metaAbstract._concreteToMap.get( new Integer( identityId ));
+
+			final MetaMap concrete = _metaAbstract._concreteToMap.get( new Integer( identityId ));
 	        if ( concrete == null )
 	        {
-	        	throw new TypeException("type not mapped:" + type.intValue() + " " + map.getName( type.intValue() ).getFullName() ); 
+	        	throw new TypeException("type not mapped:" + type.intValue() + " " + map.getName( type.intValue() ).getFullName() );
 	        }
 			return type.intValue();
 		}
-		
+
 	}
 
 	private static class CacheEntry
@@ -342,26 +358,27 @@ implements MetaDefinition, TypeRelation
 		Integer mapId;
 		TypeWriter idWriter;
 	}
-	
+
     public static class MetaAbstractWriter
     implements TypeWriter
     {
-    	private MetaAbstract _metaAbstract;
-    	private TypeWriter _uvint28;
-		private Map<Class<?>,CacheEntry> _mapCache;
-		
-    	public MetaAbstractWriter(MetaAbstract metaAbstract, TypeWriter uvint28)
+    	private final MetaAbstract _metaAbstract;
+    	private final TypeWriter _uvint28;
+		private final Map<Class<?>,CacheEntry> _mapCache;
+
+    	public MetaAbstractWriter(final MetaAbstract metaAbstract, final TypeWriter uvint28)
     	{
     		_metaAbstract = metaAbstract;
     		_uvint28 = uvint28;
     		_mapCache = new HashMap<Class<?>,CacheEntry>();
     	}
-    	
-		public void write(TypeOutputStream out, Object o)
-		throws TypeException, IOException 
+
+		@Override
+        public void write(final TypeOutputStream out, final Object o)
+		throws TypeException, IOException
 		{
-			Class<?> clss = o.getClass();
-			CacheEntry entry = (CacheEntry) _mapCache.get(clss);
+			final Class<?> clss = o.getClass();
+			CacheEntry entry = _mapCache.get(clss);
 			if (entry == null )
 			{
 				entry = mapType(out.getTypeMap(), clss);
@@ -370,39 +387,41 @@ implements MetaDefinition, TypeRelation
 			_uvint28.write(out, entry.mapId);
 			entry.idWriter.write(out, o);
 		}
-    	
-		private CacheEntry mapType(TypeMap map, Class<?> clss)
+
+		private CacheEntry mapType(final TypeMap map, final Class<?> clss)
 		throws TypeException
 		{
 			// assumes class is bound to definition, not identity.
-			int[] ids = map.getLibrary().getId(clss);
+			final int[] ids = map.getLibrary().getId(clss);
 			if (ids.length != 1)
 			{
-				if (ids.length>1)
-					throw new TypeException("Class bound to multiple system types:" +clss.getName());
-				if (ids.length==0)
-					throw new TypeException("Class not bound to any mapped type:"+clss.getName());
+				if (ids.length>1) {
+                    throw new TypeException("Class bound to multiple system types:" +clss.getName());
+                }
+				if (ids.length==0) {
+                    throw new TypeException("Class not bound to any mapped type:"+clss.getName());
+                }
 			}
-			int defId = ids[0];
-			
-			MetaName name = map.getLibrary().getName(defId);
-			
+			final int defId = ids[0];
+
+			final MetaName name = map.getLibrary().getName(defId);
+
 			// get the identity id.
-			int id = map.getLibrary().getTypeId(name);
-			
+			final int id = map.getLibrary().getTypeId(name);
+
 			// Find the identifier for the abstract map definition using identity internal id.
-			MetaMap metaMap = (MetaMap) _metaAbstract._concreteToMap.get( new Integer( id ));
+			final MetaMap metaMap = _metaAbstract._concreteToMap.get( new Integer( id ));
 	        if ( metaMap == null )
 	        {
 				throw new TypeException( "MetaAbstract - Definition '" + name.getFullName() + "' not mapped to abstract type '"  + map.getLibrary().getName(_metaAbstract.getMemberTypeId()).getFullName() + "'" );
 			}
-	        
-	        CacheEntry entry = new CacheEntry();
-	        
+
+	        final CacheEntry entry = new CacheEntry();
+
 	        if (!metaMap.isHidden)
 	        {
 		        // This is very important.  When working as a remote client we must ensure
-		        // that the server also has the mapping from concrete to map value.  By 
+		        // that the server also has the mapping from concrete to map value.  By
 		        // getting the System Id of the MapId it will automatically resolve if the
 		        // server has the correct value.
 	        	try
@@ -410,7 +429,7 @@ implements MetaDefinition, TypeRelation
 	        		map.getStreamId(metaMap.mapTypeId);
 	        		map.getStreamId(id);
 	        	}
-	        	catch (TypeException ex)
+	        	catch (final TypeException ex)
 	        	{
 	        		throw new TypeException( "MetaAbstract - Definition '" + name.getFullName() + "' not mapped to abstract type '"  + map.getLibrary().getName(_metaAbstract.getMemberTypeId()).getFullName() + "'" );
 	        	}
@@ -421,19 +440,19 @@ implements MetaDefinition, TypeRelation
 	        {
 	        	entry.mapId = new Integer(map.getStreamId(defId));
 	        }
-	        catch( TypeException ex)
+	        catch( final TypeException ex)
 	        {
-	        	MetaVersion version = map.getLibrary().getVersion(defId);
-        		throw new TypeException( "MetaAbstract - Definition '" + name.getFullName() + "/"+ version.toString() + "' not mapped to abstract type '"  + map.getLibrary().getName(_metaAbstract.getMemberTypeId()).getFullName() + "'" );	        	
+	        	final MetaVersion version = map.getLibrary().getVersion(defId);
+        		throw new TypeException( "MetaAbstract - Definition '" + name.getFullName() + "/"+ version.toString() + "' not mapped to abstract type '"  + map.getLibrary().getName(_metaAbstract.getMemberTypeId()).getFullName() + "'" );
 	        }
 	        // Cache the writer function.
 	        entry.idWriter = map.getWriter(map.getStreamId( defId ));
 			return entry;
 		}
-		
+
     }
- 
-	public boolean isMapped( int id )
+
+	public boolean isMapped( final int id )
 	{
 		return _concreteToMap.get( new Integer(id))==null?false:true;
 	}

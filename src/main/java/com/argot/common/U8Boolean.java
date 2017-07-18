e73aa5bbe02e8db528426472d4d0929b0a2f9dc0
David Ryan <daivd@future-grid.com.au>
2017-07-18 11:26:44 +1000

    Replace new Boolean with Boolean.FALSE and Boolean.TRUE to reduce memory usage.

--- src/main/java/com/argot/common/U8Boolean.java
+++ src/main/java/com/argot/common/U8Boolean.java
@@ -34,39 +34,36 @@ import com.argot.TypeReader;
 import com.argot.TypeWriter;
 
 /**
- * The BOOL type is a single U8 character which has the value 0 or 1.
- * if the actual value is anything other than 0 the final object will
- * be true.
- * 
+ * The BOOL type is a single U8 character which has the value 0 or 1. if the actual value is anything other than 0 the final object will be true.
+ *
  * It requires/returns a java.lang.Boolean
  */
-public class U8Boolean
-implements TypeReader, TypeWriter
+public class U8Boolean implements TypeReader, TypeWriter
 {
 	public static final String TYPENAME = "boolean";
 	public static final String VERSION = "1.3";
-	
-	public Object read(TypeInputStream in ) 
-	throws TypeException, IOException
+
+	public Object read(final TypeInputStream in) throws TypeException, IOException
 	{
 		int a;
-	
+
 		a = in.read();
-	
-		return new Boolean( a == 0 ? false : true );
+
+		return (a == 0 ? Boolean.FALSE : Boolean.TRUE);
 	}
 
-	public void write(TypeOutputStream out, Object o ) 
-	throws TypeException, IOException
+	public void write(final TypeOutputStream out, final Object o) throws TypeException, IOException
 	{
 		int a;
-		if ( o instanceof Boolean )
+		if (o instanceof Boolean)
 		{
-			a = ((Boolean) o).booleanValue() == false ? 0 : 1 ;
+			a = ((Boolean) o).booleanValue() == false ? 0 : 1;
 		}
 		else
-			throw new TypeException( "U8Boolean requires Boolean object");
+		{
+			throw new TypeException("U8Boolean requires Boolean object");
+		}
 
-		out.getStream().write( a );
-	}	
+		out.getStream().write(a);
+	}
 }

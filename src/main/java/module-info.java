fac6350cc7d72e1454bd4f2ae29c65eb1e65f110
David Ryan <david@future-grid.com.au>
2019-09-11 12:43:03 +1000

    messed up module name.  fixed.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -23,7 +23,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-module org.argotsdk {
+module com.argot {
 
     exports com.argot;
     exports com.argot.auto;
